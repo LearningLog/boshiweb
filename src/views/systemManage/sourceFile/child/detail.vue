840aaa414348c50331b860b7acaad46d2590d61f
wanglimei <wanglimei@cdvcloud.com>
2019-10-09 20:06:17 +0800

    文件来源

--- src/views/systemManage/sourceFile/child/detail.vue
+++ src/views/systemManage/sourceFile/child/detail.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="tenant-add">
     <el-form ref="form" :model="form" label-width="120px">
-      <el-form-item  label="文件代码">
+      <el-form-item label="文件代码">
         <el-input v-model="form.code" placeholder="请输入系统代码" /><span class="tip">文件代码,长度2-64位字符</span>
       </el-form-item>
-      <el-form-item  label="文件名称">
+      <el-form-item label="文件名称">
         <el-input v-model="form.name" placeholder="请输入文件名称" /><span class="tip">文件名称,长度2-64位字符</span>
       </el-form-item>
-      <el-form-item  label="是否启用">
+      <el-form-item label="是否启用">
         <el-radio-group v-model="form.enable_status">
           <el-radio value="1" label="启用" />
           <el-radio value="2" label="停用" />
@@ -20,24 +20,24 @@
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        form: {
-          code: '',
-          name: '',
-          enable_status: 1
-        }
-
+export default {
+  data() {
+    return {
+      form: {
+        code: '',
+        name: '',
+        enable_status: 1
       }
-    },
-    created() {
-    },
-    methods: {
 
     }
+  },
+  created() {
+  },
+  methods: {
 
   }
+
+}
 </script>
 
 <style lang="scss" scoped>
