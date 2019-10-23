293358009293b81bb9b6deeb3b8e4a5e12627b42
YanHuaKang <1060553506@qq.com>
2019-10-23 14:46:40 +0800

    1，修改默认page title

--- vue.config.js
+++ vue.config.js
@@ -7,7 +7,7 @@ function resolve(dir) {
   return path.join(__dirname, dir)
 }
 
-const name = defaultSettings.title || 'CDV CLOUD' // page title
+const name = defaultSettings.title || '博识' // page title
 
 // If your port is set to 80,
 // use administrator privileges to execute the command line.
