02124c429f116059ed657e31377f84a8429fa48a
YanHuaKang <1060553506@qq.com>
2019-11-22 13:17:02 +0800

    1，知识分类

---
+++ src/views/system-setting/knowledge-classification/index.vue
@@ -0,0 +1,115 @@
+<template>
+  <div class="app-container">
+    <p>小提示：最多可创建2个一级主题哦，主题可输入2-8个字符</p>
+    <el-tree
+      :data="treeData"
+      node-key="id"
+      default-expand-all
+      :expand-on-click-node="false"
+    >
+      <span slot-scope="{ node, data }" class="custom-tree-node">
+        <span>{{ node.label }}</span>
+        <span>
+          <el-button
+            type="text"
+            size="mini"
+            @click="() => append(data)"
+          >
+            Append
+          </el-button>
+          <el-button
+            type="text"
+            size="mini"
+            @click="() => remove(node, data)"
+          >
+            Delete
+          </el-button>
+        </span>
+      </span>
+    </el-tree>
+  </div>
+</template>
+
+<script>
+let id = 1000
+export default {
+  data() {
+    const treeData = [{
+      id: 1,
+      label: '一级 1',
+      children: [{
+        id: 4,
+        label: '二级 1-1',
+        children: [{
+          id: 9,
+          label: '三级 1-1-1'
+        }, {
+          id: 10,
+          label: '三级 1-1-2'
+        }]
+      }]
+    }, {
+      id: 2,
+      label: '一级 2',
+      children: [{
+        id: 5,
+        label: '二级 2-1'
+      }, {
+        id: 6,
+        label: '二级 2-2'
+      }]
+    }, {
+      id: 3,
+      label: '一级 3',
+      children: [{
+        id: 7,
+        label: '二级 3-1'
+      }, {
+        id: 8,
+        label: '二级 3-2'
+      }]
+    }]
+    return {
+      dataIsChange: 0, // 计数器，据此判断表单是否已编辑
+      noLeaveprompt: false, // 表单提交后，设置为true，据此判断提交不再弹出离开提示
+      treeData: treeData
+    }
+  },
+  watch: {
+    // 监听表单数据变化
+    treeData: {
+      handler(val) {
+        if (val) {
+          this.dataIsChange++
+        }
+      },
+      deep: true
+    }
+  },
+  created() {
+
+  },
+  methods: {
+    append(data) {
+      console.log('data', data)
+      const newChild = { id: id++, label: 'testtest', children: [] }
+      if (!data.children) {
+        this.$set(data, 'children', [])
+      }
+      data.children.push(newChild)
+    },
+
+    remove(node, data) {
+      console.log(node, data)
+      const parent = node.parent
+      const children = parent.data.children || parent.data
+      const index = children.findIndex(d => d.id === data.id)
+      children.splice(index, 1)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
