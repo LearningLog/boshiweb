f3959eae94aa725272f912eeeba45e15ce031175
高顺 <gaoshun@cdvcloud.com>
2019-11-27 18:18:59 +0800

    fix #5478134 优化用户管理详情展示

---
+++ src/components/User/groupList.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="user-group-list">
+    <span v-for="item in data" :key="item._id" class="group-list-box">
+        <span>{{item.groupName}}</span>
+        <span class="manage-tips" v-if="item.manage">负责人</span>
+    </span>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'GroupList',
+  props: {
+    data: {
+      type: Array,
+      default: function() {
+        return []
+      }
+    }
+  },
+  data() {
+    return {}
+  },
+  computed: {
+  },
+  methods: {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .manage-tips {
+    font-size: 12px;
+  }
+
+  .group-list-box {
+    display: inline-block;
+    margin-right: 10px;
+  }
+</style>
