160dd65cb5c54e3568c188c6993d951cc40f82da
高顺 <gaoshun@cdvcloud.com>
2019-11-27 15:27:52 +0800

    fix #5516687 将换成session

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -5,18 +5,16 @@
 * @remarks:
 */
 
-import Cookies from 'js-cookie'
-
 const TokenKey = 'boshi_token'
 
 export function getToken() {
-  return Cookies.get(TokenKey)
+  return sessionStorage.getItem(TokenKey)
 }
 
 export function setToken(token) {
-  return Cookies.set(TokenKey, token)
+  return sessionStorage.setItem(TokenKey, token)
 }
 
 export function removeToken() {
-  return Cookies.remove(TokenKey)
+  return sessionStorage.removeItem(TokenKey)
 }
