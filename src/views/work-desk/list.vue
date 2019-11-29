b42abdc14fc4de3122cd26f10591be505498d4e2
zhaoxiaoke <zhaoxiaoke@cdvcloud.com>
2019-11-29 22:06:37 +0800

    优化

--- src/views/work-desk/list.vue
+++ src/views/work-desk/list.vue
@@ -434,13 +434,19 @@ export default {
     download(row) {
       const fileId = this.getFileListData(row.mainFileId)._id
       const url = process.env.VUE_APP_BASE_API + '/api/workDeskFile/downloadFile?fileId=' + fileId
-      const a = document.createElement('a')
+      const elemIF = document.createElement('iframe')
+      elemIF.src = url
+      elemIF.style.display = 'none'
+      document.body.appendChild(elemIF)
+      elemIF.loadData()
+      document.body.removeChild(elemIF)
+      /* const a = document.createElement('a')
       a.download = row.fileName + '\.' + row.fileFormat
       a.href = url
       document.body.appendChild(a)
       a.click()
       URL.revokeObjectURL(a.href)
-      document.body.removeChild(a)
+      document.body.removeChild(a)*/
     },
     // 批量下载
     batchDownload() {
@@ -461,13 +467,12 @@ export default {
           type: 'warning'
         }).then(() => {
           const url = process.env.VUE_APP_BASE_API + '/api/workDeskFile/downloadZipFile?token=' + token
-          const a = document.createElement('a')
-          a.download = '批量下载.zip'
-          a.href = url
-          document.body.appendChild(a)
-          a.click()
-          URL.revokeObjectURL(a.href)
-          document.body.removeChild(a)
+          const elemIF = document.createElement('iframe')
+          elemIF.src = url
+          elemIF.style.display = 'none'
+          document.body.appendChild(elemIF)
+          elemIF.loadData()
+          document.body.removeChild(elemIF)
         }).catch(() => {})
       })
     },
